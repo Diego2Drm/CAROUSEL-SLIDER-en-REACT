f0d06680425b2df212c58e9a8d8ca47432f380d0
Diego Ramirez <diego.ramirez2d03@gmail.com>
2024-12-16 19:52:45 -0600

    selección de imagen

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,14 @@ function App() {
   const listRef = useRef();
 
   // el index actual del item
-  const [currentIdex, setCurrentIndex ] = useState(0);
+  const [currentIdex, setCurrentIndex] = useState(0);
 
 
   useEffect(() => {
     const listNode = listRef.current;
     const imgNode = listNode.querySelectorAll("li > img")[currentIdex];
 
-    if(imgNode){
+    if (imgNode) {
       imgNode.scrollIntoView({
         behavior: "smooth"
       })
@@ -21,18 +21,21 @@ function App() {
   }, [currentIdex]);
 
   const scrollToImage = (direction) => {
-    if( direction === 'prev'){
-      setCurrentIndex( curr => { 
+    if (direction === 'prev') {
+      setCurrentIndex(curr => {
         const firstSlide = currentIdex === 0;
         return firstSlide ? 0 : curr - 1;
       })
     } else {
-      const isLastSlide = currentIdex === data.length -1 ;
-      if(!isLastSlide){
-        setCurrentIndex( curr => curr + 1)
+      const isLastSlide = currentIdex === data.length - 1;
+      if (!isLastSlide) {
+        setCurrentIndex(curr => curr + 1)
       }
     }
+  }
 
+  const goToSlide = (slideIndex) => {
+    setCurrentIndex(slideIndex);
   }
 
   return (
@@ -48,13 +51,25 @@ function App() {
               {
                 data.map((item) => {
                   return <li key={item.id}>
-                    <img src={item.imgUrl} alt="images" width={500} height={280}/>
+                    <img src={item.imgUrl} alt="images" width={700} height={320} />
                   </li>
                 })
               }
 
             </ul>
           </div>
+          <div className="dots-container">
+            {
+              // data.map(( _, idx) => (
+              data.map(( item, idx) => (
+                <div key={idx}
+                  className={`dot-container-item ${idx === currentIdex ? "active" : " "}`}
+                  onClick={() => goToSlide(idx)}>
+                  <img src={item.imgUrl} alt="" width={100} height={50}/>
+                </div>))
+            }
+          </div>
+
         </div>
       </section>
     </React.Fragment>
@@ -62,3 +77,15 @@ function App() {
 }
 
 export default App
+
+
+{/* <div className="dots-container">
+{
+  data.map((_, idx) => (
+    <div key={idx}
+      className={`dot-container-item ${idx === currentIndex ? "active" : ""}`}
+      onClick={() => goToSlide(idx)}>
+      &#9865;
+    </div>))
+}
+</div> */}
